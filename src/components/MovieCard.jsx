7ef601921ba84d1bd731eab1065d37b8ef4c581c
kim-ho-seon <kmad910119@gmail.com>
2024-10-07 15:37:27 +0900

    mission3 오타수정

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,88 +3,78 @@ import '../App.css'
 import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
 
-const StyledHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  background-color: #111;
-  color: white;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 1000;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  height: 80px;
-
-  h1 {
-    font-size: 3.5rem;
-    color: #fff;
-
-    a {
-      text-decoration: none;
-      color: inherit;
-    }
-  }
-
-  ul {
-    list-style: none;
+const StyledCard = styled.div`
+box-sizing: border-box;
+padding-top: 5em;
+ul { 
     display: flex;
-    margin: 0;
-    padding: 0;
-
-    li {
-      display: flex;
-      align-items: center;
-
-      a, input {
-        margin-left: 0.3rem;
-        height: 30px;
-        font-size: 0.8rem;
-        font-weight: 500;
-        border: none;
-        border-radius: 7px;
+    flex-wrap: wrap;
+    gap: .1rem;
+    justify-content: center;
+    
+    li{
+      width: 200px;
+      /* overflow: hidden; */
+      position: relative;
+      padding: .5rem;
+      
+      img{
+        width: 100%;
+        object-fit: cover;
+        aspect-ratio: 8 / 12; /* 가로 8, 세로 12의 비율 유지 */
+        border-radius: 1rem;
+        transition: transform 0.3s ease;
+        
         cursor: pointer;
-        text-decoration: none;
-      }
 
-      a {
+        &:hover{
+          transform: scale(1.05); /* 호버 시 이미지 1.1배 확대 */
+          box-shadow: 0 5px 8px rgba(0, 0, 0, 0.2)
+        }
+      }
+     
+      div{
+        padding: .6rem;
         display: flex;
         align-items: center;
-        justify-content: center;
-        color: white;
-        background-color: #fabf0e;
-        width: 65px;
-      }
-
-      a:hover {
-        background-color: #e2b83a;
-      }
-
-      input {
-        padding: 0.5rem;
+        justify-content: space-between;
+      h3{ 
+        white-space: nowrap; /* 줄 바꿈 방지 */
+        overflow: hidden; /* 넘치는 내용 숨김 */
+        text-overflow: ellipsis; /* 넘치는 내용에 ... 표시 */
+        }
+        span{
+          color: #fabf0e;
+        }
       }
     }
   }
 
-  /* 미디어 쿼리 (480px 이하) */
-  @media (max-width: 480px) {
-    ul li a {
-      /* 첫 번째 링크(Dark Mode)의 텍스트를 🌙로 변경 */
-      &:nth-child(1) {
-        &::after {
-          content: '🌙';
+    @media screen and (max-width: 768px){
+ul { 
+    li{
+      width: 150px;
+      div{
+        padding: .3rem;
+      h3{ 
+        padding-right: 0;
+        font-size: 1rem;
+          }
         }
-        &::before {
-          content: '';
-        }
-        font-size: 1.5rem;
-        background-color: transparent; /* 배경색 없애기 (원한다면) */
       }
     }
-  }
-`;
 
+    @media (max-width: 480px){
+      ul { 
+    li{
+      width: 350px;
+
+      }
+    }
+      
+    }
+  }
+`
 
 export default function MovieCard({ searchResults }) {
   const [movies, setMovies] = useState([]); // 초기 영화 상태 (빈 배열로 설정)
